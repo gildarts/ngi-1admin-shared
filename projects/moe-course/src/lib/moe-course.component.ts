90ecab42249c57f8b7e6988090583dfb40b1d526
Yao Ming Huang <yaoming.huang@ischool.com.tw>
2021-01-23 23:32:28 +0800

    使用 npm web-gadget 呼叫 service。

--- projects/moe-course/src/lib/moe-course.component.ts
+++ projects/moe-course/src/lib/moe-course.component.ts
@@ -1,20 +1,34 @@
+import { GadgetService } from '@1campus/web-gadget';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'lib-moe-course',
   template: `
-    <p>
-      moe-course works!
-    </p>
+    <div>
+      <pre>{{schoolInfo | json}}</pre>
+    </div>
   `,
   styles: [
   ]
 })
 export class MoeCourseComponent implements OnInit {
 
-  constructor() { }
+  accessPoint?: string;
+  schoolInfo: any;
 
-  ngOnInit(): void {
+  constructor(
+    private gadget: GadgetService
+  ) { }
+
+  async ngOnInit() {
+    const contract = await this.gadget.getContract('basic.public');
+
+    this.accessPoint = contract.getAccessPoint;
+
+    // 呼叫 service。
+    this.schoolInfo = await contract.send('beta.GetSystemConfig', {
+      Name: '學校資訊'
+    });
   }
 
 }
--- projects/moe-course/src/lib/moe-course.module.ts
+++ projects/moe-course/src/lib/moe-course.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { MoeCourseComponent } from './moe-course.component';
-
+import { WebGadgetModule } from '@1campus/web-gadget';
+import { CommonModule } from '@angular/common';
 
 
 @NgModule({
   declarations: [MoeCourseComponent],
   imports: [
+    CommonModule,
+    WebGadgetModule
   ],
   exports: [MoeCourseComponent]
 })
