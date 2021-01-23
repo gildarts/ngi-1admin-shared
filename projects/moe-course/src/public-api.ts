82f345421e9975619cdf9064a04a70662d302795
Yao Ming Huang <yaoming.huang@ischool.com.tw>
2021-01-24 01:17:53 +0800

    公開測試。

--- projects/moe-course/src/public-api.ts
+++ projects/moe-course/src/public-api.ts
@@ -2,6 +2,25 @@
  * Public API Surface of moe-course
  */
 
-export * from './lib/course-code-table';
-export * from './lib/graduation-plan/graduation-plan';
+ /** 課程規劃相關 */
+ export * from './lib/graduation-plan/graduation-plan';
+ export * from './lib/graduation-plan/common';
+ export * from './lib/graduation-plan/subject-group';
+ export * from './lib/graduation-plan/subject';
+
+ /** 課程代碼相關 */
+ export * from './lib/course-code-table';
+ export * from './lib/code-line';
+ export * from './lib/course-code-record';
+ export * from './lib/course-code.service';
+ export * from './lib/course-code';
+ export * from './lib/course-type-map';
+ export * from './lib/credit_set';
+ export * from './lib/diff-subject';
+ export * from './lib/field';
+ export * from './lib/mapping-table';
+ export * from './lib/moe.service';
+ export * from './lib/subject-key';
+ export * from './lib/unified-subject';
+
 export * from './lib/moe-course.module';
