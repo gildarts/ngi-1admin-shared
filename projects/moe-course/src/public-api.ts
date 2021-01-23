767cdfbf86bf1f7401b06c364e41a44381f47c8f
Yao Ming Huang <yaoming.huang@ischool.com.tw>
2021-01-24 00:37:00 +0800

    測試可以正常。

--- projects/moe-course/src/public-api.ts
+++ projects/moe-course/src/public-api.ts
@@ -2,4 +2,6 @@
  * Public API Surface of moe-course
  */
 
+export * from './lib/course-code-table';
+export * from './lib/graduation-plan/graduation-plan';
 export * from './lib/moe-course.module';
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MOEService } from 'projects/moe-course/src/public-api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'nglib-moe-course';
+
+  codes: any;
+
+  constructor(
+    private moe: MOEService
+  ) {}
+
+  async ngOnInit() {
+    this.codes = await this.moe.getCourseCodeTable('108041305H11101A');
+  }
 }
