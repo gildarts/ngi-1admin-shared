9097ce9c6847e83a935c0432b9b9ab528d9f2692
Yao Ming Huang <yaoming.huang@ischool.com.tw>
2021-04-07 18:48:14 +0800

    完成代碼對照表機制。

--- projects/moe-course/src/lib/node/apply-description.ts
+++ projects/moe-course/src/lib/node/apply-description.ts
@@ -1,5 +1,6 @@
 import { Field } from "../common/code-field";
 import { CourseCode } from "../common/course-code";
+import { ListCode } from "../common/list-code";
 import { MappingTable } from "../common/mapping-table";
 
 /** 套用課程代碼各欄位說明。 */
@@ -22,4 +23,33 @@ export async function applyDescription(codeList: CourseCode[], mappingTables: Ma
             code.setDescription(field, map, factor);
         }
     }
+}
+
+/** 載入代碼對照表。 */
+export function loadMapCollection(codeTableRaw: { name: string, content: any }[]) {
+
+    const argList = [
+        { name: ListCode.SCH, field: Field.N03, keyName: '代碼', valueName: '代碼說明' },
+        { name: ListCode.GRP, field: Field.N04, keyName: '代碼', valueName: '代碼說明', factorName: '適用課程類型' },
+        { name: ListCode.DEP, field: Field.N05, keyName: '代碼', valueName: '代碼說明', factorName: '適用課程類型' },
+        { name: ListCode.CLA, field: Field.N06, keyName: '代碼', valueName: '代碼說明', factorName: '適用課程類型' },
+        { name: ListCode.CAT, field: Field.N07, keyName: '代碼', valueName: '代碼說明' },
+        // 開課方式在新規格是每學期可以不同，需另外處理。
+        // { name: ListCode.MOD, field: Field.N08, keyName: '代碼', valueName: '代碼說明' },
+        { name: ListCode.ATT1, field: Field.N09, keyName: '代碼', valueName: '代碼說明' },
+        { name: ListCode.FLD, field: Field.N10, keyName: '代碼', valueName: '代碼說明', factorName: '適用課程類型' },
+    ]
+
+    const mapraw: any = codeTableRaw.map(m => [m.name as string, m.content as any]);
+    const mapDatas = new Map<string, any>(mapraw);
+
+    const mapcoll = new Map<Field, MappingTable>();
+    for (const arg of argList) {
+        const mapdata = mapDatas.get(arg.name);
+        const map = new MappingTable(arg.keyName, arg.valueName, mapdata, arg.factorName);
+
+        mapcoll.set(arg.field, map);
+    }
+
+    return mapcoll;
 }
--- src/app/node-test/node-test.component.ts
+++ src/app/node-test/node-test.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { loadMapCollection } from 'projects/moe-course/src/lib/node/apply-description';
 import { CourseCode, Field, ListCode, MappingTable } from 'projects/moe-course/src/public-api';
 import { ApiResponse, CodeTables } from './example';
 
@@ -15,30 +16,31 @@ export class NodeTestComponent implements OnInit {
 
     ngOnInit(): void {
 
-        const argList = [
-            { name: ListCode.SCH, field: Field.N03, keyName: '代碼', valueName: '代碼說明' },
-            { name: ListCode.GRP, field: Field.N04, keyName: '代碼', valueName: '代碼說明', factorName: '適用課程類型' },
-            { name: ListCode.DEP, field: Field.N05, keyName: '代碼', valueName: '代碼說明', factorName: '適用課程類型' },
-            { name: ListCode.CLA, field: Field.N06, keyName: '代碼', valueName: '代碼說明', factorName: '適用課程類型' },
-            { name: ListCode.CAT, field: Field.N07, keyName: '代碼', valueName: '代碼說明' },
-            // 開課方式在新規格是每學期可以不同，需另外處理。
-            // { name: ListCode.MOD, field: Field.N08, keyName: '代碼', valueName: '代碼說明' },
-            { name: ListCode.ATT1, field: Field.N09, keyName: '代碼', valueName: '代碼說明' },
-            { name: ListCode.FLD, field: Field.N10, keyName: '代碼', valueName: '代碼說明', factorName: '適用課程類型' },
-        ]
+        const mapcoll = loadMapCollection(CodeTables);
+        this.data = mapcoll.get(Field.N09)?.getDescription('B');
 
-        const mapraw: any = CodeTables.map(m => [m.name as string, m.content as any]);
-        const mapDatas = new Map<string, any>(mapraw);
+        // const argList = [
+        //     { name: ListCode.SCH, field: Field.N03, keyName: '代碼', valueName: '代碼說明' },
+        //     { name: ListCode.GRP, field: Field.N04, keyName: '代碼', valueName: '代碼說明', factorName: '適用課程類型' },
+        //     { name: ListCode.DEP, field: Field.N05, keyName: '代碼', valueName: '代碼說明', factorName: '適用課程類型' },
+        //     { name: ListCode.CLA, field: Field.N06, keyName: '代碼', valueName: '代碼說明', factorName: '適用課程類型' },
+        //     { name: ListCode.CAT, field: Field.N07, keyName: '代碼', valueName: '代碼說明' },
+        //     // 開課方式在新規格是每學期可以不同，需另外處理。
+        //     // { name: ListCode.MOD, field: Field.N08, keyName: '代碼', valueName: '代碼說明' },
+        //     { name: ListCode.ATT1, field: Field.N09, keyName: '代碼', valueName: '代碼說明' },
+        //     { name: ListCode.FLD, field: Field.N10, keyName: '代碼', valueName: '代碼說明', factorName: '適用課程類型' },
+        // ]
 
-        const mapcoll = new Map<Field, MappingTable>();
-        for (const arg of argList) {
-            const mapdata = mapDatas.get(arg.name);
-            const map = new MappingTable(arg.keyName, arg.valueName, mapdata, arg.factorName);
+        // const mapraw: any = CodeTables.map(m => [m.name as string, m.content as any]);
+        // const mapDatas = new Map<string, any>(mapraw);
 
-            mapcoll.set(arg.field, map);
-        }
+        // const mapcoll = new Map<Field, MappingTable>();
+        // for (const arg of argList) {
+        //     const mapdata = mapDatas.get(arg.name);
+        //     const map = new MappingTable(arg.keyName, arg.valueName, mapdata, arg.factorName);
 
-        this.data = mapcoll.get(Field.N09)?.getDescription('A');
+        //     mapcoll.set(arg.field, map);
+        // }
 
     }
 
